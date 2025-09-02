d9030d577e149c1e84439a69c7e8001c691e7c62
hello-freind <henry.fitzpatrick2@education.nsw.gov.au>
2025-09-02 10:07:31 +1000

    hdlfksj;lkl;
    fsljkda;lfkj;lfkj
    i guess it works

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ def index():
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5100)
+
+
+@app.route('/newpage')
+def new_page():
+  return render_template('homepage.html')
+
