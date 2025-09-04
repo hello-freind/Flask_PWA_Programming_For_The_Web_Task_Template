26e62b3c613b07e49fe99d5bffac333825cf5d5b
hello-freind <henry.fitzpatrick2@education.nsw.gov.au>
2025-09-04 11:29:42 +1000

    woahspdlifh;aksjduofiayvg

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ import database_manager as dbHandler
 
 app = Flask(__name__)
 
-@app.route('/index.html', methods=['GET'])
+@app.route('/homepagep.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
-  return render_template('/index.html')
+  return render_template('/homepage.html')
+
+@app.route('/add.html', methods=['GET'])
+def add():
+  return render_template('/add.html')  
+
+@app.route('/about.html', methods=['GET'])
+def about():
+  return render_template('/about.html')
+
+@app.route('/otherpage.html', methods=['GET'])
+def otherpage():
+  return render_template('/otherpage.html')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5100)
 
-
-@app.route('/newpage')
-def new_page():
-  return render_template('homepage.html')
 
